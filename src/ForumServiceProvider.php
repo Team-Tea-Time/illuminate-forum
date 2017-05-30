3c86c78d6d303de0c9dea012ed1b726028297c01
Andreas <andreaselia@live.co.uk>
2017-05-30 16:29:25 +0100

    Basic service provider

---
+++ src/ForumServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AndreasElia\Forum;
+
+use AndreasElia\Forum\Models\Discussion;
+use AndreasElia\Forum\Models\Group;
+use AndreasElia\Forum\Models\Post;
+
+class ForumServiceProvider extends ServiceProvider
+{
+    // 
+}
