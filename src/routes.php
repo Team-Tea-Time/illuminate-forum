ee3e20c1ad1457f58b3f8ee38d7a03b8eda260b6
Rakhmatdzhon Abdurashidov <rahmatjon.abdurashidov@gmail.com>
2017-06-02 15:14:45 +0500

    added routes (#10)
    * added routes
    
    * Remove spacing

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,16 @@ Route::prefix(config('forum.prefix'))->group(function () {
     Route::get('/', function () {
         return 'Hallo Welt';
     });
+
+    Route::post('groups/create', 'GroupController@store');
+    Route::put('groups/{id}', 'GroupController@update');
+    Route::delete('groups/{id}', 'GroupController@destroy');
+
+    Route::post('discussions/create', 'DiscussionController@store');
+    Route::put('discussions/{id}', 'DiscussionController@update');
+    Route::delete('discussions/{id}', 'DiscussionController@destroy');
+
+    Route::post('posts/create', 'PostController@store');
+    Route::put('posts/{id}', 'PostController@update');
+    Route::delete('posts/{id}', 'PostController@destroy');
 });
