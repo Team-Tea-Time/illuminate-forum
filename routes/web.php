4233b3cde75e2e81b3dda7688429cd132db72408
Marco Aurélio Deleu <deleugyn@gmail.com>
2017-06-13 20:05:27 +0200

    [Hotfix] Revert changes on routes (#67)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-Route::group(function () {
+Route::group(['prefix' => config('forum.prefix'), 'name' => 'forum.', 'namespace' => config('forum.namespace')], function () {
     Route::resource('groups', 'GroupController', ['store', 'update', 'destroy']);
     Route::resource('discussions', 'DiscussionController', ['store', 'update', 'destroy']);
     Route::resource('posts', 'PostController', ['store', 'update', 'destroy']);
 
     Route::resource('lock', 'Discussion\LockController', ['store', 'destroy']);
     Route::resource('sticky', 'Discussion\StickyController', ['store', 'destroy']);
-})->prefix(config('forum.prefix'))->name('forum.')->namespace(config('forum.namespace'));
+});
