a748b6717abf1ca449f1b80072c2485b966018a8
Julien G <j.gelmar@hotmail.de>
2017-06-27 22:34:10 +0200

    Add german translations (#144)
    * add german translations
    
    * add missing translation

---
+++ resources/lang/de/forum.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    // General
+    'forum'                         => 'Forum',
+    'sr_nav_label'                  => 'Navigation wechseln',
+
+    // Groups
+    'groups_plural'                 => 'Gruppen',
+    'groups_singular'               => 'Gruppe',
+
+    // Dicussions
+    'discussions_plural'            => 'Diskussionen',
+    'discussions_singular'          => 'Diskussion',
+    'create_discussion'             => 'Diskussion erstellen',
+    'edit_discussion'               => 'Diskussion bearbeiten',
+    'new_discussion'                => 'Neue Diskussion',
+    'no_discussions_group_notice'   => 'Es gibt keine Diskussionen in dieser Gruppe.',
+    'no_discussions_general_notice' => 'Es gibt keine Diskussionen.',
+
+    // Posts
+    'posts_plural'                  => 'Beiträge',
+    'posts_singular'                => 'Beitrag',
+    'edit_post'                     => 'Beitrag bearbeiten',
+
+    // Discussion Single
+    'lock_discussion'               => 'Sperren',
+    'stick_discussion'              => 'Anheften',
+
+    // Labels
+    'label_title'                   => 'Titel',
+    'label_content'                 => 'Inhalt',
+
+    // Inputs
+    'input_reply'                   => 'Schreibe deinen Beitrag hier...',
+    'input_search'                  => 'Forum durchsuchen',
+
+    // Buttons
+    'submit'                        => 'Absenden',
+    'save'                          => 'Speichern',
+    'unlock'                        => 'Entsperren',
+    'lock'                          => 'Sperren',
+    'unsticky'                      => 'Entheften',
+    'sticky'                        => 'Anheften',
+
+    // Search
+    'search_results'                => 'Ergebnisse durchsuchen',
+    'no_search_results'             => 'Keine Ergebnisse gefunden.',
+
+];
