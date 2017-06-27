aa296a3b799f0f20197f66fff4d5eff7800fafd7
Gideon Brett <gideonb234@users.noreply.github.com>
2017-06-27 18:39:07 +0100

    korean tl (checked by at least one korean as well) (#142)

---
+++ resources/lang/ko-kr/forum.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    // General
+    'forum'                         => '게시판', // Forum
+    'sr_nav_label'                  => '메뉴 숨기기/보이기', // Toggle Navigation
+
+    // Groups
+    'groups_plural'                 => '그룹', // Groups
+    'groups_singular'               => '그룹', // Group
+
+    // Dicussions
+    'discussions_plural'            => '토론', // Discussion
+    'discussions_singular'          => '토론', // Discussion
+    'create_discussion'             => '토론 만들기', // Create Discussion
+    'edit_discussion'               => '토론 수정하기', // Edit Discussion
+    'new_discussion'                => '새 토론', // New Discussion
+    'no_discussions_group_notice'   => '이 그룹에 토론이 없습니다.', // No discussions to display for this group
+    'no_discussions_general_notice' => '토론이 없습니다.', // No discussions to display
+
+    // Posts
+    'posts_plural'                  => '게시물', // Posts
+    'posts_singular'                => '게시물', // Post
+    'edit_post'                     => '게시물 수정하기', // Edit Post
+
+    // Discussion Single
+    'lock_discussion'               => '토론 잠금하기', // Lock
+    'stick_discussion'              => '토론 공지로 등록하기', // Sticky
+
+    // Labels
+    'label_title'                   => '제목', // Title
+    'label_content'                 => '게시물', // Content
+
+    // Inputs
+    'input_reply'                   => '응답 입력', // Enter your reply here
+    'input_search'                  => '게시판에서 검색', // Search Forum
+
+    // Buttons
+    'submit'                        => '보내기', // Submit
+    'save'                          => '저장하기', // Save
+    'unlock'                        => '잠금해제', // Unlock
+    'lock'                          => '잠금하기', // Lock
+    'unsticky'                      => '공지 등록해제', // Unsticky
+    'sticky'                        => '공지로 등록하기', // Sticky
+
+    // Search
+    'search_results'                => '검색결과', // Search Results
+    'no_search_results'             => '검색결과가 없습니다.', // No search results to display
+
+];
