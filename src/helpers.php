f01859a11cd585b05b28fc18ba2b6cbde9485682
Andreas Elia <andreaselia@live.co.uk>
2017-07-02 06:16:44 +0000

    Apply fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,11 @@
 <?php
 
-if (! function_exists('is_group_mode')) {
+if (!function_exists('is_group_mode')) {
     /**
      * Check if the group mode equals what was specified.
      *
-     * @param  string  $mode
+     * @param string $mode
+     *
      * @return bool
      */
     function is_group_mode(string $mode)
