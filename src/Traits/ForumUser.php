9d9a4764b43fd834e963b4172bf2a514f4a3295c
Tom Irons <tom.irons@hotmail.com>
2017-07-02 03:08:14 -0400

    user-relationships (#167)
    * Add german translations (#144)
    
    * add german translations
    
    * add missing translation
    
    * add posts relation and doc blocks
    
    * fix conflict

--- src/Traits/ForumUser.php
+++ src/Traits/ForumUser.php
@@ -3,11 +3,27 @@
 namespace Bitporch\Forum\Traits;
 
 use Bitporch\Forum\Models\Discussion;
+use Bitporch\Forum\Models\Post;
 
 trait ForumUser
 {
+    /**
+     * Return all the discussions created by the user.
+     *
+     * @return mixed
+     */
     public function discussions()
     {
         return $this->hasMany(Discussion::class);
+    }
+
+    /**
+     * Get all the posts created by the user.
+     *
+     * @return mixed
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
     }
 }
